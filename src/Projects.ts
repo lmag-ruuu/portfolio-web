bfbe8be977cdc1bfa905c298bbea6d31e60bed99
lmagruu <magg4ms@gmail.com>
2022-02-01 13:23:52 -0400

    order projects

--- src/Projects.ts
+++ src/Projects.ts
@@ -1,5 +1,29 @@
 const Projects = [
   {
+    image: "/static/portfolio.webp",
+    technology: ["nextjs", "chakra", "typescript"],
+    title: "Portfolio",
+    link: "https://github.com/lmag-ruuu/portfolio-web",
+    description:
+      "My personal portfolio, I used chakraui and nextjs, I had a lot of fun and focus my knowledge on the front end.",
+  },
+  {
+    image: "/static/great-quotes.webp",
+    technology: ["nextjs", "typescript", "material-ui"],
+    title: "Quotes",
+    link: "https://great-quotes-omega.vercel.app/",
+    description:
+      "An app to add quotes and comment them, for this one I used mongodb, nextjs, material-ui and typescript, it was really a challenge, I have very little knowledge with mongodb.",
+  },
+  {
+    image: "/static/meetups.webp",
+    technology: ["typescript", "nextjs", "css"],
+    title: "Meetups",
+    link: "https://nextjs-meetup-demo-five.vercel.app/",
+    description:
+      "A small application for adding and viewing descriptions of well-known places. My first look with nextjs.",
+  },
+  {
     image: "/static/pokesearch.webp",
     technology: ["html", "css", "javascript"],
     title: "PokeSearch",
@@ -22,30 +46,6 @@ const Projects = [
     link: "https://lmagruu-omnifood.netlify.app/",
     description:
       "One of the final projects done in Jonas' HTML course, is a fake startup food service, was the beginning of everything.",
-  },
-  {
-    image: "/static/portfolio.webp",
-    technology: ["nextjs", "chakra", "typescript"],
-    title: "Portfolio",
-    link: "https://github.com/lmag-ruuu/portfolio-web",
-    description:
-      "My personal portfolio, I used chakraui and nextjs, I had a lot of fun and focus my knowledge on the front end.",
-  },
-  {
-    image: "/static/meetups.webp",
-    technology: ["typescript", "nextjs", "css"],
-    title: "Meetups",
-    link: "https://nextjs-meetup-demo-five.vercel.app/",
-    description:
-      "A small application for adding and viewing descriptions of well-known places. My first look with nextjs.",
-  },
-  {
-    image: "/static/great-quotes.webp",
-    technology: ["nextjs", "typescript", "material-ui"],
-    title: "Quotes",
-    link: "https://great-quotes-omega.vercel.app/",
-    description:
-      "An app to add quotes and comment them, for this one I used mongodb, nextjs, material-ui and typescript, it was really a challenge, I have very little knowledge with mongodb.",
   },
 ];
 
